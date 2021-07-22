44200df910d9f44aa1ebba4b8911fe6e638b360a
Sarosh Farhan <saroshfarhan6495@gmail.com>
2021-07-22 22:53:05 +0530

    Reomoved API keys and phone numbers

--- twilioSMS.py
+++ twilioSMS.py
@@ -1,18 +1,15 @@
 from twilio.rest import Client
 # Your Account Sid and Auth Token from twilio account
-account_sid = 'ACd774fef6d0d94509288a891442d641bc'
-auth_token = 'df9fbbb7182bcc86588aa84cf16c8c17'
+account_sid = 'YOUR_ACCOUNT_SID'
+auth_token = 'YOUR_ACCOUNT_TOKEN'
 # instantiating the Client
 client = Client(account_sid, auth_token)
-
-
-# from_=+17632251011, to=+918319578097
 
 
 def sendMessage(title, desc, url):
     # sending message
     content = title + '\n' + desc + '\n' + url
     message = client.messages.create(
-        body=content, from_='whatsapp:+14155238886', to='whatsapp:+917840908066')
+        body=content, from_='whatsapp:+14155238886', to='whatsapp:+91XXXXXXXXXX')
     # printing the sid after success
     print(message.sid)
